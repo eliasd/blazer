434a2027f632ff8817b93bdc9bae67cdaf6750ca
Daniel Guillen <eliasd.gui@gmail.com>
2023-04-01 00:58:26 -0700

    refactor dev urls

--- contents/content.tsx
+++ contents/content.tsx
@@ -10,17 +10,20 @@ export const getStyle = () => {
     return style
 }
 
+const DEV_URLS: string[] = [
+    "http://paulgraham.com/*",
+    "http://www.paulgraham.com/*",
+    "https://*.substack.com/*",
+    "https://www.theverge.com/*",
+    "https://www.youtube.com/*",
+    "https://www.wsj.com/*",
+    "https://www.ycombinator.com/*",
+    "https://super-static-assets.s3.amazonaws.com/*",
+]
+
 export const config: PlasmoCSConfig = {
-    matches: [
-        "http://paulgraham.com/*",
-        "http://www.paulgraham.com/*",
-        "https://*.substack.com/*",
-        "https://www.theverge.com/*",
-        "https://www.youtube.com/*",
-        "https://www.wsj.com/*",
-        "https://www.ycombinator.com/*",
-        "https://super-static-assets.s3.amazonaws.com/*",
-    ],
+    matches: DEV_URLS,
+    exclude_matches: [],
     all_frames: true
 }
 
